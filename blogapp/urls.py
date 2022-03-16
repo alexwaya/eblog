136a1a69b6aae92288784cd6403cff41409dfb80
Alexander Waya Nguku <alex.waya@gmail.com>
2022-03-17 02:19:13 +0300

    update custom code files

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,7 +1,7 @@
+from django.contrib import admin
 from django.urls import path
-
-from . import views
-
+from .views import *
+ 
 urlpatterns = [
     path('blogs/', BlogListView, name='blogs'),
     path('blog/<int:_id>', BlogDetailView, name='blog'),
